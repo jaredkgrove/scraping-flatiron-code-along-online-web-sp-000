c20521b4fd574d148d5685628bbc741584e7704a
Jared Grove <jaredkgrove@gmail.com>
2019-04-24 01:30:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,12 +16,23 @@ class Scraper
     end
   end
   
- def get_page
+  def get_page
+    
+  end
+  
+  def get_courses
+    
+  end
+  
+  def make_courses
+    
+  end
+  
+  def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
  
       binding.pry
   end
- 
 end
 
 Scraper.new.get_page
