512a7efbb866119f1fcee582f0556dc29d212f04
Jared Grove <jaredkgrove@gmail.com>
2019-04-24 01:23:17 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -4,6 +4,14 @@ class Course
   @@all = []
   
   def initialize
-    @@all << self
+    @@all << self 
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.reset_all
+    @@all.clear
   end
 end
